48447538031e6b5bd3f7fbf79b16ed59ebbc5e27
Farshad <farshadhassanpoor@gmail.com>
2024-01-27 17:24:21 +0330

    complete task

---
+++ todolist/src/App.jsx
@@ -0,0 +1,31 @@
+import { useState } from "react";
+import "./App.css";
+const Id =  ()=>Math.floor(Math.random()*1000);
+function App() {
+  const [name, setName] = useState("");
+  const [list,setList] =useState([]);
+  const handleOnchange = ({target:{value}}) =>setName(value);
+  const newData = {name,id:Id()}
+const handleOnsubmit = ()=>{
+  setName("");
+  setList([...list,newData]);
+}
+const handleDelete = (id)=>setList((notes)=>notes.filter((n)=> n.id !== id ));
+
+  return (
+    <div>
+      <input type="text" value={name} onChange={handleOnchange} />
+      <button onClick={handleOnsubmit}>save</button>
+      {list.map(({id,name})=>(
+        <ul key={id}>
+          <li>{name}</li>
+          <button onClick={()=>handleDelete(id)}>X</button>
+        </ul>
+
+      ))}
+
+    </div>
+  )
+}
+
+export default App;
